2d4f87b094f233590c1a28966cb32d373be5524b
Michael Rao <jmichaelrao@gmail.com>
2021-08-07 18:09:59 -0500

    update api routes structure with skeleton for methods. add notes for putting in the cream filling

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,40 @@
 const db = require("../models");
+const router = require("express").Router();
 
 // get workouts
+router
+  .get("/api/workouts", (req, res) => {
+    // cream filling
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 
 // add exercise
+router
+    .put("/api/workouts", (req, res) => {
+      // cream filling
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 
 // create workout
+router
+  .post("/api/workout", (req, res) => {
+    // cream filling
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 
 // get workouts in range
+router
+  .get("/api/workouts/range", (req, res) => {
+    // cream filling
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 
 module.exports = router;
