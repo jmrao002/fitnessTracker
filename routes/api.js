9a0cec38b8cd9a61d31f62518d72cf8988d7e471
Michael Rao <jmichaelrao@gmail.com>
2021-08-07 20:58:23 -0500

    completed api routes

--- routes/api.js
+++ routes/api.js
@@ -2,39 +2,51 @@ const db = require("../models");
 const router = require("express").Router();
 
 // get workouts
-router
-  .get("/api/workouts", (req, res) => {
-    // cream filling
-  })
-  .catch((err) => {
-    res.json(err);
-  });
-
-// add exercise
-router
-    .put("/api/workouts", (req, res) => {
-      // cream filling
-  })
-  .catch((err) => {
-    res.json(err);
-  });
-
-// create workout
-router
-  .post("/api/workout", (req, res) => {
-    // cream filling
-  })
-  .catch((err) => {
-    res.json(err);
-  });
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .sort({ date: -1 })
+    .then((dbworkout) => {
+      res.json(dbworkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 // get workouts in range
-router
-  .get("/api/workouts/range", (req, res) => {
-    // cream filling
-  })
-  .catch((err) => {
-    res.json(err);
-  });
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .sort({ date: -1 })
+    .then((dbworkout) => {
+      res.json(dbworkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// add exercise
+router.put("/api/workouts/:id", (req, res) => {
+  const body = req.body;
+  const id = req.params.id;
+  db.Workout.findByIdAndUpdate({ _id: id }, { exercises: body })
+    .then((dbworkout) => {
+      res.json(dbworkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// create workout
+router.post("/api/workouts", (req, res) => {
+  db.Workout.create(req.body)
+    .then((dbworkout) => {
+      res.json(dbworkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
